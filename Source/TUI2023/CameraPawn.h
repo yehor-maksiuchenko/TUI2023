71669ce903e81af30954cbea76b8dfcca8fd9788
yehorkk <keynikus@gmail.com>
2023-12-17 17:58:57 +0100

    3DPlayer Controller v.01
    Forgot to create different branch for this, so let it just be in this branch

--- Source/TUI2023/CameraPawn.h
+++ Source/TUI2023/CameraPawn.h
@@ -5,6 +5,7 @@
 #include "Camera/CameraComponent.h"
 #include "Components/SceneComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 #include "Components/ArrowComponent.h"
 #include "CameraPawn.generated.h"
 
@@ -19,19 +20,22 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-	void TurnCamera(float Value);
+	/*void RotateHorizontal(float Value);
+	void RotateVertical(float Value);*/
 	void MoveForward(float Value);
 	void MoveRight(float Value);
-	void LiftCamera(float Value);
-
-	const float LowestCameraHight = 200.0f;
-	const float HighestCameraHight = 1000.0f;
+	void Zoom(float Value);
+	void RotateRight(float Value);
+	void RotateLeft(float Value);
+	/*void EnableRotation();
+	void DisableRotation();*/
 
 public:	
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		UCameraComponent* CamComp;
 
@@ -43,4 +47,41 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float MinZoom = 200.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float MaxZoom = 4000.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float ZoomSpeed = 2.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float MoveSpeed = 20.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float RotateSpeed = 2.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float PitchMax = 80.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float PitchMin = 10.0f;
+
+	/*UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool bCanRotate = false;*/
+
+private:
+	UFUNCTION()
+		void CameraBounds();
+
+	UPROPERTY()
+		FVector TargetLocation;
+
+	UPROPERTY()
+		FRotator TargetRotation;
+
+	UPROPERTY()
+		float TargetZoom;
 };
